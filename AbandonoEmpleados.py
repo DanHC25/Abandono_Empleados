fd0dd30a04c24256e53c5e70c6f23f0e42c330cb
DanHC25 <danilo.huaman25@gmail.com>
2024-05-02 21:33:52 -0500

    Impacto Económico

--- AbandonoEmpleados.py
+++ AbandonoEmpleados.py
@@ -1,6 +1,7 @@
 # Cargar librerías
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Cargar datos de archivo csv
 df = pd.read_csv('AbandonoEmpleados.csv', sep = ';', index_col= 'id', na_values='#N/D')
@@ -125,4 +126,44 @@ plt.show()
     3. Trabaja en ventas
     4. Bajo salario
     5. Alta carga de horas extras '''
+
+
+## Impacto Económico de las Renuncias ##
+
+''' Según el estudio "Cost of Turnover" del 'Center for American Progress':
+    1. El coste de la fuga de los empleados que ganan menos de $30000 es del 16,1% de su salario
+    2. El coste de la fuga de los empleados que ganan entre $30000-$50000 es del 19,7% de su salario
+    3. El coste de la fuga de los empleados que ganan entre $50000-$75000 es del 20,4% de su salario
+    4. El coste de la fuga de los empleados que ganan más de $75000 es del 21% de su salario '''
+
+# Creamos una nueva variable salario_anual del empleado
+df['salario_anual'] = df["salario_mes"].transform(lambda x: x*12)
+df[['salario_mes','salario_anual']]
+
+# Calculamos el impacto económico de cada empleado si deja la empresa
+condiciones = [(df['salario_anual'] <= 30000),
+               (df['salario_anual'] > 30000) & (df['salario_anual'] <= 50000),
+               (df['salario_anual'] > 50000) & (df['salario_anual'] <= 75000),
+               (df['salario_anual'] > 75000)]
+
+#Lista de resultados
+resultados = [df["salario_anual"] * 0.161, df["salario_anual"] * 0.197, df["salario_anual"] * 0.204, df["salario_anual"] * 0.21]
+
+#Aplicamos select
+df['impacto_abandono'] = np.select(condiciones,resultados, default = -999)
+
+# ¿Cúanto nos ha costado este problema en el último año?
+coste_total =  df.loc[df.abandono == 1].impacto_abandono.sum()
+coste_total # Costo total de $2719005.912
+
+# ¿Cuanto nos cuesta que los empleados no estén motivados? (pérdidas en implicación == Baja)
+df.loc[(df.abandono == 1) & (df.implicacion == 'Baja')].impacto_abandono.sum()
+
+# ¿Cuanto dinero podríamos ahorrar fidelizando mejor a nuestros empleados?
+print(f"Reducir un 10% la fuga de empleados nos ahorraría {int(coste_total * 0.1)}$ cada año.") # $271900
+print(f"Reducir un 20% la fuga de empleados nos ahorraría {int(coste_total * 0.2)}$ cada año.") # $543801
+print(f"Reducir un 30% la fuga de empleados nos ahorraría {int(coste_total * 0.3)}$ cada año.") # $815701
+
+
+
 
